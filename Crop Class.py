df80fef6349ae73ab362f76b6932330bc2dc7be9
kestrel150 <30732@longroad.ac.uk>
2014-06-13 13:34:20 +0100

    Crop Class - 3
    -Menu functionality now supported.

--- Crop Class.py
+++ Crop Class.py
@@ -78,14 +78,54 @@ def manual_grow(crop):
             print("Please enter a valid value between 1 and 10.")
 
     crop.grow(light,water)
-    
+
+def display_menu():
+    print("1. Grow manually over 1 day")
+    print("2. Grow automatically over 30 days")
+    print("3. Report status")
+    print("0. Exit test program")
+    print()
+    print("Please select an option from the above menu.")
+
+def get_menu_choice():
+    option_valid = False
+    while not option_valid:
+        try:
+            choice = int(input("Option Selected: "))
+            if 0 <= choice <= 4:
+                option_valid = True
+            else:
+                print("Please enter a valid option")
+        except ValueError:
+            print("Please enter a valid option")
+    return choice
+
+def manage_crop(crop):
+    print("This is the crop management program")
+    print()
+    noexit = True
+    while noexit:
+        display_menu()
+        option = get_menu_choice()
+        print()
+        if option == 1:
+            manual_grow(crop)
+            print()
+        elif option == 2:
+            auto_grow(crop,30)
+            print()
+        elif option == 3:
+            print(crop.report())
+            print()
+        elif option == 0:
+            noexit = False
+            print()
+    print("Thank you for using this crop management program.")
+
 def main():
     new_crop = Crop(1,4,3)
-    print(new_crop.needs()) # can specify specific attribute to return by []
-    print(new_crop.report())
-
-    manual_grow(new_crop)
-    print(new_crop.report())
+    manage_crop(new_crop)
+    
 
 
 if __name__ == "__main__":

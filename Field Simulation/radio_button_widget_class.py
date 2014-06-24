042a6056826a9b705a583e6c1d86eb551ac2688b
kestrel150 <30732@longroad.ac.uk>
2014-06-24 15:36:02 +0100

    Moved Radio button class
    radio button class moved to field simulation

---
+++ Field Simulation/radio_button_widget_class.py
@@ -0,0 +1,52 @@
+#Aaron Parker
+#170614
+#RadioButtonWidget
+
+from PyQt4.QtGui import *
+
+class RadioButtonWidget(QWidget):
+    """Creates a group of radio buttons from a given list of labels"""
+
+    #constructor
+    def __init__(self, label, instruction, button_list):
+        super().__init__()
+
+        #widgets
+        self.title_label = QLabel(label)
+        self.radio_group_box = QGroupBox(instruction)
+        self.radio_button_group = QButtonGroup()
+
+        #list to store radio buttons
+        self.radio_button_list = []
+        for each in button_list:
+            self.radio_button_list.append(QRadioButton(each))
+            
+        #set default values
+        self.radio_button_list[0].setChecked(True)
+
+        #create layout for radio buttons
+        self.radio_button_layout = QVBoxLayout()
+
+        #add buttons to layout & button group
+        counter = 1
+        for each in self.radio_button_list:
+            self.radio_button_layout.addWidget(each)
+            self.radio_button_group.addWidget(each)
+            self.radio_button_group.setId(each, counter)
+            counter += 1
+
+        #add radio button to group box
+        self.radio_group_box.setLayout(self.radio_button_layout)
+
+        #create layout for entire widget
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addWidget(self.title_label)
+        self.main_layout.addWidget(self.radio_group_box)
+
+        #set layout for widget
+        self.setLayout(self.main_layout)
+
+    def selected_button(self):
+        return self.radio_button_group.checkId()
+        
+        
